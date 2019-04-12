d51ffe6283e4a5c42fb3242ae6835354b58dcc69
Michael Kim <mkim@devmini-01.avtr.net>
2019-04-11 17:43:55 -0700

    add photos to app

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './HomePage.scss'
+import Img from 'react-image'
 
 const HomePage = () => {
     return <div className="HomePage">
         <h1>Nick & Emily</h1>
         <p>May 8th, 2020 - New Orleans</p>
-        </div>
+        <Img src={"https://i.imgur.com/Fynn9e2.jpg"} alt="nick and emily" />
+    </div>
 }
 
 export default HomePage
