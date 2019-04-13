c75b75b800513cbae1a96076c72cd7d132947e98
mikebkim <mikebkim@gmail.com>
2019-04-12 23:02:17 -0700

    nothing important

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
         <h1>Nick & Emily</h1>
         <p>May 8th, 2020 - New Orleans</p>
         <Img className="home-image" src={"https://i.imgur.com/Fynn9e2.jpg"} alt="nick and emily" />
-        <h2>- Our Story -</h2>
+        <h2>Our Story</h2>
     </div>
 }
 
