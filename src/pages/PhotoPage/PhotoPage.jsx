19af3b1f38fde1016f68b0a98b6e72a6e3c27a41
Michael Kim <mikebkim@gmail.com>
2022-11-08 15:45:07 -0800

    adding submit photo

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PhotoPage.scss'
 
 const PhotoPage = () => {
+
+    const [newComment, setNewComment] = useState('')
+    const [textValue, setTextValue] = useState('')
+
+    function addComment(text) {
+        setNewComment(text)
+    }
+
     return <div className="PhotoPage">
         <h1>Photos</h1>
+        <form>
+            <input
+                type={'text'}
+                value={textValue}
+                onChange={(e) => setTextValue(e.target.value)}
+            />
+        </form>
+        <div
+            onClick={() => addComment(textValue)}
+        >
+            Submit
+        </div>
+        {newComment}
     </div>
 }
 
